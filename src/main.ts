219449f99746a8fe03edcab5fb3921429d92229a
lebrancconvas <lebrancconvas@gmail.com>
2025-06-29 13:29:00 +0700

    ✨ feat: update portfolio.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,2 @@
 import gsap from "gsap";
 import $ from "jquery";
-
-
-// GSAP
-const box = document.createElement("div");
-box.style.width = "100px";
-box.style.height = "100px";
-box.style.backgroundColor = "green";
-document.body.appendChild(box);
-
-gsap.to(box, {
-  rotation: 360,
-  duration: 3,
-  repeat: -1,
-  ease: "none"
-});
-
-console.log(`GSAP is working at version: ${gsap.version}`);
-
-// JQUERY
-
-$(function() {
-  console.log(`jQuery is working at version: ${$.fn.jquery}`);
-})
-
